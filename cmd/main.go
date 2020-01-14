6d9702e6b0624246db5cf6738159ab652451af92
Camilo Acuña <camilo.aku@gmail.com>
2020-01-14 02:46:02 -0300

    implement messages-lib support in cmd file

--- cmd/main.go
+++ cmd/main.go
@@ -2,22 +2,13 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 
-	mc "github.com/microapis/clients-go/messages"
-	"github.com/microapis/messages-api/backend"
-	"github.com/microapis/messages-api/channel"
-	backendEmail "github.com/microapis/messages-email-api/backend"
-	"github.com/microapis/messages-email-api/provider"
-)
-
-var (
-	ses      *provider.SESProvider
-	sendgrid *provider.SendgridProvider
-	mandrill *provider.MandrillProvider
+	backendIot "github.com/microapis/messages-iot-api/backend"
+	"github.com/microapis/messages-lib/service"
 )
 
 func main() {
@@ -28,83 +19,65 @@ func main() {
 	if providersEnv == "" {
 		log.Fatal(errors.New("PROVIDERS value not defined"))
 	}
-
 	// define provider slice names
-	ppn := strings.Split(providersEnv, ",")
-	// define providers slice
-	pp := make([]*channel.Provider, 0)
+	providers := strings.Split(providersEnv, ",")
 
-	// iterate over providers name
-	for _, v := range ppn {
-		switch v {
-		case provider.SESName:
-			ses = provider.NewSES()
-			err = ses.LoadEnv()
-			pp = append(pp, ses.ToProvider())
-		case provider.SendgridName:
-			sendgrid = provider.NewSendgrid()
-			err = sendgrid.LoadEnv()
-			pp = append(pp, sendgrid.ToProvider())
-		case provider.MandrillName:
-			mandrill = provider.NewMandrill()
-			err = mandrill.LoadEnv()
-			pp = append(pp, mandrill.ToProvider())
-		}
-	}
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// read messages-api env values
-	messagesHost := os.Getenv("MESSAGES_HOST")
-	if messagesHost == "" {
-		log.Fatal(errors.New("MESSAGES_HOST value not defined"))
-	}
-	messagesPort := os.Getenv("MESSAGES_PORT")
-	if messagesPort == "" {
-		log.Fatal(errors.New("MESSAGES_PORT value not defined"))
-	}
-
-	// register channel on messages-api
-	addr := fmt.Sprintf("%s:%s", messagesHost, messagesPort)
-
-	host := os.Getenv("HOST")
-	if host == "" {
-		log.Fatal(errors.New("HOST value not defined"))
-	}
-
-	// get grpc port env value:
+	// get grpc port env value
 	port := os.Getenv("PORT")
 	if port == "" {
 		err := errors.New("invalid PORT env value")
 		log.Fatal(err)
 	}
 
-	// create channel to register
-	c := &channel.Channel{
-		Name:      "email",
-		Host:      host,
-		Port:      port,
-		Providers: pp,
+	// get redis host env value
+	redisHost := os.Getenv("REDIS_HOST")
+	if redisHost == "" {
+		err := errors.New("invalid REDIS_HOST env value")
+		log.Fatal(err)
 	}
 
-	MessagesAPI := mc.NewService(addr)
-	err = MessagesAPI.Register(c)
+	// get redis port env value
+	redisPort := os.Getenv("REDIS_PORT")
+	if redisPort == "" {
+		err := errors.New("invalid REDIS_PORT env value")
+		log.Fatal(err)
+	}
+
+	// get redis database env value
+	redisDatabase := os.Getenv("REDIS_DATABASE")
+	if redisDatabase == "" {
+		err := errors.New("invalid REDIS_DATABASE env value")
+		log.Fatal(err)
+	}
+	rd, err := strconv.Atoi(redisDatabase)
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatal(err)
 	}
 
-	log.Println("Channel email is registered on messages-api with providers:", c.ProvidersNames())
+	// initialice message backend with Approve and Deliver methods
+	backend, err := backendIot.NewBackend(providers)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// define address value to grpc service
-	addr = fmt.Sprintf("0.0.0.0:%s", port)
+	// initialize message service
+	svc, err := service.NewMessageService("Email", service.ServiceConfig{
+		Port: port,
 
-	// define service with Approve and Deliver methods
-	svc := backendEmail.NewBackend(sendgrid, mandrill, ses)
+		RedisHost:     redisHost,
+		RedisPort:     redisPort,
+		RedisDatabase: rd,
 
-	// start grpc pigeon-ses-api service
-	log.Printf("Serving at %s", addr)
-	if err := backend.ListenAndServe(addr, svc); err != nil {
+		Approve: backend.Approve,
+		Deliver: backend.Deliver,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// run message service
+	err = svc.Run()
+	if err != nil {
 		log.Fatal(err)
 	}
 }
