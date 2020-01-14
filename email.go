10e68761bb6644f75f5478cfc647901ff259e4cd
Camilo Acuña <camilo.aku@gmail.com>
2020-01-14 02:44:46 -0300

    add email struct

--- email.go
+++ email.go
@@ -1,1 +1,18 @@
-package email
+package messagesemail
+
+const (
+	// Channel ...
+	Channel = "email"
+)
+
+// Message ...
+type Message struct {
+	From     string   `json:"from"`
+	FromName string   `json:"from_name"`
+	To       []string `json:"to"`
+	ReplyTo  []string `json:"reply_to"`
+	Subject  string   `json:"subject"`
+	Text     string   `json:"text"`
+	HTML     string   `json:"html"`
+	Provider string   `json:"provider"`
+}
