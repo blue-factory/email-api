33a7b8815a495c6fba36af9e6fd090080df4476f
Camilo Acuña <camilo.aku@gmail.com>
2020-01-14 02:45:47 -0300

    add message-lib support

--- backend/backend.go
+++ backend/backend.go
@@ -6,8 +6,15 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/microapis/messages-api"
+	messagesemail "github.com/microapis/messages-email-api"
 	"github.com/microapis/messages-email-api/provider"
+	"github.com/microapis/messages-lib/channel"
+)
+
+var (
+	sendgrid *provider.SendgridProvider
+	mandrill *provider.MandrillProvider
+	ses      *provider.SESProvider
 )
 
 // Backend ...
@@ -18,16 +25,42 @@ type Backend struct {
 }
 
 // NewBackend ...
-func NewBackend(sendgrid *provider.SendgridProvider, mandrill *provider.MandrillProvider, ses *provider.SESProvider) *Backend {
+func NewBackend(providers []string) (*Backend, error) {
+	var err error
+
+	// define providers slice
+	pp := make([]*channel.Provider, 0)
+
+	// iterate over providers name
+	for _, v := range providers {
+		switch v {
+		case provider.SESName:
+			ses = provider.NewSES()
+			err = ses.LoadEnv()
+			pp = append(pp, ses.ToProvider())
+		case provider.SendgridName:
+			sendgrid = provider.NewSendgrid()
+			err = sendgrid.LoadEnv()
+			pp = append(pp, sendgrid.ToProvider())
+		case provider.MandrillName:
+			mandrill = provider.NewMandrill()
+			err = mandrill.LoadEnv()
+			pp = append(pp, mandrill.ToProvider())
+		}
+	}
+	if err != nil {
+		return nil, err
+	}
+
 	return &Backend{
 		Sendgrid: sendgrid,
 		Mandrill: mandrill,
 		Ses:      ses,
-	}
+	}, nil
 }
 
 // Approve ...
-func (s *Backend) Approve(content string) (valid bool, err error) {
+func (b *Backend) Approve(content string) (valid bool, err error) {
 	log.Println(fmt.Sprintf("[gRPC][MessagesEmailService][Approve][Request] content = %v", content))
 
 	if content == "" {
@@ -36,7 +69,7 @@ func (s *Backend) Approve(content string) (valid bool, err error) {
 		return false, err
 	}
 
-	m := new(messages.Email)
+	m := new(messagesemail.Message)
 	err = json.Unmarshal([]byte(content), m)
 	if err != nil {
 		log.Println(fmt.Sprintf("[gRPC][MessagesEmailService][Approve][Error] error = %v", err))
@@ -45,11 +78,11 @@ func (s *Backend) Approve(content string) (valid bool, err error) {
 
 	switch m.Provider {
 	case provider.SendgridName:
-		err = s.Sendgrid.Approve(m)
+		err = b.Sendgrid.Approve(m)
 	case provider.MandrillName:
-		err = s.Mandrill.Approve(m)
+		err = b.Mandrill.Approve(m)
 	case provider.SESName:
-		err = s.Ses.Approve(m)
+		err = b.Ses.Approve(m)
 	}
 	if err != nil {
 		log.Println(fmt.Sprintf("[gRPC][MessagesEmailService][Approve][Error] error = %v", err))
@@ -62,7 +95,7 @@ func (s *Backend) Approve(content string) (valid bool, err error) {
 }
 
 // Deliver ...
-func (s *Backend) Deliver(content string) error {
+func (b *Backend) Deliver(content string) error {
 	log.Println(fmt.Sprintf("[gRPC][MessagesEmailService][Deliver][Request] content = %v", content))
 
 	if content == "" {
@@ -71,7 +104,7 @@ func (s *Backend) Deliver(content string) error {
 		return err
 	}
 
-	m := new(messages.Email)
+	m := new(messagesemail.Message)
 	err := json.Unmarshal([]byte(content), m)
 	if err != nil {
 		log.Println(fmt.Sprintf("[gRPC][MessagesEmailService][Deliver][Error] error = %v", err))
@@ -80,11 +113,11 @@ func (s *Backend) Deliver(content string) error {
 
 	switch m.Provider {
 	case provider.SendgridName:
-		err = s.Sendgrid.Deliver(m)
+		err = b.Sendgrid.Deliver(m)
 	case provider.MandrillName:
-		err = s.Mandrill.Deliver(m)
+		err = b.Mandrill.Deliver(m)
 	case provider.SESName:
-		err = s.Ses.Deliver(m)
+		err = b.Ses.Deliver(m)
 	}
 	if err != nil {
 		log.Println(fmt.Sprintf("[gRPC][MessagesEmailService][Deliver][Error] error = %v", err))
